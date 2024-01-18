2eb3802f2141e3fdf78f8c50da07df35ad4861b9
prabhat511 <prabhat.bhargav@informa.com>
2024-01-18 21:32:56 +0530

    removed client code

--- swiftcart-client/src/app/interfaces/cartitem.interface.ts
+++
@@ -1,5 +0,0 @@
-import { IProduct } from "./product.interface"
-export interface ICartItem {
-    quantity: number,
-    productId: IProduct
-};
--- swiftcart-client/src/app/interfaces/order.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IOrder {
-    userId: string,
-    totalAmount: number,
-    items: Array<any>,
-    shippingAddress: string,
-    status: string
-}
