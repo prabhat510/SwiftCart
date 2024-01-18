a3f075b0c0960c3b0fa68b90e4a1f6bc2a2dada0
prabhat511 <prabhat.bhargav@informa.com>
2024-01-18 20:53:35 +0530

    feat: upload build

--- swiftcart-client/src/app/utils/api.config.ts
+++ swiftcart-client/src/app/utils/api.config.ts
@@ -2,17 +2,17 @@ export function getServiceUrl(env?: string) {
     switch (env) {
       case "dev":
         return {
-          swiftCartApiEndpoint: "http://localhost:3000/api",
+          swiftCartApiEndpoint: "https://swiftcart-production.up.railway.app/api",
           authApiEndpoint: "https://jwt-authservice-production.up.railway.app/api/auth",
         };
       case "prod":
         return {
-          swiftCartApiEndpoint: "http://localhost:3000/api",
+          swiftCartApiEndpoint: "https://swiftcart-production.up.railway.app/api",
           authApiEndpoint: "https://jwt-authservice-production.up.railway.app/api/auth",
         };
       default:
         return {
-          swiftCartApiEndpoint: "http://localhost:3000/api",
+          swiftCartApiEndpoint: "https://swiftcart-production.up.railway.app/api",
           authApiEndpoint: "https://jwt-authservice-production.up.railway.app/api/auth",
         };
     }
